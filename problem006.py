a264433549937c2f6017fe1162ac1f64a28ea664
Felix Febrian <f.febrian96@gmail.com>
2020-12-21 15:03:37 +0100

    Finish problem 7

--- problem006.py
+++ problem006.py
@@ -11,9 +11,11 @@ Find the difference between the sum of the squares of the first one hundred natu
 bers and the square of the sum.
 """
 
+
 def test_find_difference_sum_of_squares_and_square_of_sum():
     assert find_difference_sum_of_squares_and_square_of_sum(10) == 2640
     assert find_difference_sum_of_squares_and_square_of_sum(100) == 25164150
+
 
 def find_difference_sum_of_squares_and_square_of_sum(num: int) -> int:
     """Find the difference between sum of squares and square of sum for the first
@@ -25,7 +27,7 @@ def find_difference_sum_of_squares_and_square_of_sum(num: int) -> int:
     Returns:
         int: Difference.
     """
-    sum_of_squares = sum(x ** 2 for x in range(1, num+1, 1))
-    square_of_sum = sum(range(1, num+1, 1)) ** 2
+    sum_of_squares = sum(x ** 2 for x in range(1, num + 1, 1))
+    square_of_sum = sum(range(1, num + 1, 1)) ** 2
 
     return square_of_sum - sum_of_squares
--- problem007.py
+++ problem007.py
@@ -4,3 +4,27 @@ prime is 13.
 
 What is the 10001st prime number?
 """
+
+from math import log
+
+
+def test_find_nth_prime_number():
+    assert find_nth_prime_number(6) == 13
+    assert find_nth_prime_number(10001) == 104743  # Solution to Problem!
+
+
+def find_nth_prime_number(n: int) -> int:
+    """Find the nth prime number.
+
+    Args:
+        n (int): index of the prime number
+
+    Returns:
+        int: the nth prime number
+    """
+    upper_bound = int(n * (log(n) + log(log(n))))
+    primes = list(range(2, upper_bound))
+    for idx in range(int(n ** (0.5) + 1)):
+        primes = list(filter(lambda x: x % primes[idx] or x == primes[idx], primes))
+
+    return primes[n - 1]
